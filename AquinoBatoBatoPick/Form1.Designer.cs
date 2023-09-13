affcad69b48001e5bb36cd9325bbb916ea2e020e
kyanahh <kiannadominiquealvarez@gmail.com>
2023-09-13 15:55:00 +0800

    first commit

---
+++ AquinoBatoBatoPick/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace AquinoBatoBatoPick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblBBP = new System.Windows.Forms.Label();
+            this.lblLet = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblComputer = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblComputerScore = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblUserScore = new System.Windows.Forms.Label();
+            this.rdbBato = new System.Windows.Forms.RadioButton();
+            this.rdbPapel = new System.Windows.Forms.RadioButton();
+            this.rdbGunting = new System.Windows.Forms.RadioButton();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(154)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.panel1.Controls.Add(this.btnPlay);
+            this.panel1.Controls.Add(this.rdbGunting);
+            this.panel1.Controls.Add(this.rdbPapel);
+            this.panel1.Controls.Add(this.rdbBato);
+            this.panel1.Controls.Add(this.lblBBP);
+            this.panel1.Controls.Add(this.lblLet);
+            this.panel1.Controls.Add(this.lblResult);
+            this.panel1.Controls.Add(this.lblComputer);
+            this.panel1.Controls.Add(this.lblScore);
+            this.panel1.Controls.Add(this.lblComputerScore);
+            this.panel1.Controls.Add(this.lblUser);
+            this.panel1.Controls.Add(this.lblUserScore);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(539, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblBBP
+            // 
+            this.lblBBP.AutoSize = true;
+            this.lblBBP.Font = new System.Drawing.Font("Alien Encounters", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBBP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(88)))));
+            this.lblBBP.Location = new System.Drawing.Point(69, 62);
+            this.lblBBP.Name = "lblBBP";
+            this.lblBBP.Size = new System.Drawing.Size(411, 38);
+            this.lblBBP.TabIndex = 1;
+            this.lblBBP.Text = "BATO BATO PICK";
+            // 
+            // lblLet
+            // 
+            this.lblLet.AutoSize = true;
+            this.lblLet.Font = new System.Drawing.Font("Alien Encounters", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLet.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(88)))));
+            this.lblLet.Location = new System.Drawing.Point(131, 31);
+            this.lblLet.Name = "lblLet";
+            this.lblLet.Size = new System.Drawing.Size(282, 21);
+            this.lblLet.TabIndex = 2;
+            this.lblLet.Text = "Let\'s Play A Game!";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Alien Encounters", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(88)))));
+            this.lblResult.Location = new System.Drawing.Point(23, 362);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 17);
+            this.lblResult.TabIndex = 3;
+            // 
+            // lblComputer
+            // 
+            this.lblComputer.AutoSize = true;
+            this.lblComputer.Font = new System.Drawing.Font("Alien Encounters", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(88)))));
+            this.lblComputer.Location = new System.Drawing.Point(339, 266);
+            this.lblComputer.Name = "lblComputer";
+            this.lblComputer.Size = new System.Drawing.Size(121, 17);
+            this.lblComputer.TabIndex = 5;
+            this.lblComputer.Text = "Computer";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Alien Encounters", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(88)))));
+            this.lblScore.Location = new System.Drawing.Point(46, 266);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(90, 17);
+            this.lblScore.TabIndex = 6;
+            this.lblScore.Text = "Score : ";
+            // 
+            // lblComputerScore
+            // 
+            this.lblComputerScore.AutoSize = true;
+            this.lblComputerScore.Font = new System.Drawing.Font("Alien Encounters", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputerScore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(88)))));
+            this.lblComputerScore.Location = new System.Drawing.Point(385, 298);
+            this.lblComputerScore.Name = "lblComputerScore";
+            this.lblComputerScore.Size = new System.Drawing.Size(28, 22);
+            this.lblComputerScore.TabIndex = 8;
+            this.lblComputerScore.Text = "0";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Alien Encounters", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(88)))));
+            this.lblUser.Location = new System.Drawing.Point(201, 266);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(63, 17);
+            this.lblUser.TabIndex = 10;
+            this.lblUser.Text = "User";
+            // 
+            // lblUserScore
+            // 
+            this.lblUserScore.AutoSize = true;
+            this.lblUserScore.Font = new System.Drawing.Font("Alien Encounters", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserScore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(88)))));
+            this.lblUserScore.Location = new System.Drawing.Point(216, 298);
+            this.lblUserScore.Name = "lblUserScore";
+            this.lblUserScore.Size = new System.Drawing.Size(28, 22);
+            this.lblUserScore.TabIndex = 11;
+            this.lblUserScore.Text = "0";
+            // 
+            // rdbBato
+            // 
+            this.rdbBato.AutoSize = true;
+            this.rdbBato.Font = new System.Drawing.Font("Alien Encounters", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbBato.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(88)))));
+            this.rdbBato.Location = new System.Drawing.Point(43, 141);
+            this.rdbBato.Name = "rdbBato";
+            this.rdbBato.Size = new System.Drawing.Size(93, 23);
+            this.rdbBato.TabIndex = 12;
+            this.rdbBato.TabStop = true;
+            this.rdbBato.Text = "Bato";
+            this.rdbBato.UseVisualStyleBackColor = true;
+            // 
+            // rdbPapel
+            // 
+            this.rdbPapel.AutoSize = true;
+            this.rdbPapel.Font = new System.Drawing.Font("Alien Encounters", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbPapel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(88)))));
+            this.rdbPapel.Location = new System.Drawing.Point(204, 141);
+            this.rdbPapel.Name = "rdbPapel";
+            this.rdbPapel.Size = new System.Drawing.Size(107, 23);
+            this.rdbPapel.TabIndex = 13;
+            this.rdbPapel.TabStop = true;
+            this.rdbPapel.Text = "Papel";
+            this.rdbPapel.UseVisualStyleBackColor = true;
+            // 
+            // rdbGunting
+            // 
+            this.rdbGunting.AutoSize = true;
+            this.rdbGunting.Font = new System.Drawing.Font("Alien Encounters", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbGunting.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(88)))));
+            this.rdbGunting.Location = new System.Drawing.Point(363, 141);
+            this.rdbGunting.Name = "rdbGunting";
+            this.rdbGunting.Size = new System.Drawing.Size(128, 23);
+            this.rdbGunting.TabIndex = 14;
+            this.rdbGunting.TabStop = true;
+            this.rdbGunting.Text = "Gunting";
+            this.rdbGunting.UseVisualStyleBackColor = true;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(88)))));
+            this.btnPlay.Font = new System.Drawing.Font("Alien Encounters", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(154)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.btnPlay.Location = new System.Drawing.Point(43, 194);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(448, 38);
+            this.btnPlay.TabIndex = 15;
+            this.btnPlay.Text = "PLAY";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(563, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Bato Bato Pick";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblBBP;
+        private System.Windows.Forms.Label lblLet;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblComputer;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblComputerScore;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblUserScore;
+        private System.Windows.Forms.RadioButton rdbGunting;
+        private System.Windows.Forms.RadioButton rdbPapel;
+        private System.Windows.Forms.RadioButton rdbBato;
+        private System.Windows.Forms.Button btnPlay;
+    }
+}
+
---
+++ AquinoBatoBatoPick/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AquinoBatoBatoPick
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int num = rand.Next(1, 4);
+            string compChoice = "";
+
+            if (num == 1)
+            {
+                compChoice = "Bato";
+            }
+            else if (num == 2)
+            {
+                compChoice = "Papel";
+            }
+            else if (num == 3)
+            {
+                compChoice = "Gunting";
+            }
+
+            lblResult.Text = compChoice;
+
+            int computer = Convert.ToInt32(lblComputerScore.Text);
+            int user = Convert.ToInt32(lblUserScore.Text);
+
+            while (!((computer == 3 && user < computer) || (user == 3 && computer < user)))
+            {
+                if (rdbBato.Checked == true && compChoice == "Bato" || rdbPapel.Checked == true && compChoice == "Papel" || rdbGunting.Checked == true && compChoice == "Gunting")
+                {
+                    lblResult.Text = "It's a tie! Computer's choice is " + compChoice;
+                    rdbBato.Checked = false;
+                    rdbGunting.Checked = false;
+                    rdbPapel.Checked = false;
+                    return;
+                }
+                else if (rdbBato.Checked == true && compChoice == "Papel" || rdbPapel.Checked == true && compChoice == "Gunting" || rdbGunting.Checked == true && compChoice == "Bato")
+                {
+                    lblResult.Text = "Computer Wins! Computer's choice is " + compChoice;
+                    computer++;
+                    lblComputerScore.Text = computer.ToString();
+                    rdbBato.Checked = false;
+                    rdbGunting.Checked = false;
+                    rdbPapel.Checked = false;
+                    return;
+                }
+                else if (rdbBato.Checked == true && compChoice == "Gunting" || rdbPapel.Checked == true && compChoice == "Bato" || rdbGunting.Checked == true && compChoice == "Papel")
+                {
+                    lblResult.Text = "User Wins! Computer's choice is " + compChoice;
+                    user++;
+                    lblUserScore.Text = user.ToString();
+                    rdbBato.Checked = false;
+                    rdbGunting.Checked = false;
+                    rdbPapel.Checked = false;
+                    return;
+                }
+            }
+
+            if (computer == 3 && user < computer)
+            {
+                MessageBox.Show("Winner : Computer", "Congratulations", MessageBoxButtons.OK);
+                rdbBato.Checked = false;
+                rdbGunting.Checked = false;
+                rdbPapel.Checked = false;
+                computer = 0;
+                user = 0;
+                lblUserScore.Text = user.ToString();
+                lblComputerScore.Text = computer.ToString();
+            }
+            else if (user == 3 && user > computer)
+            {
+                MessageBox.Show("Winner : User", "Congratulations", MessageBoxButtons.OK);
+                rdbBato.Checked = false;
+                rdbGunting.Checked = false;
+                rdbPapel.Checked = false;
+                computer = 0;
+                user = 0;
+                lblUserScore.Text = user.ToString();
+                lblComputerScore.Text = computer.ToString();
+            }
+        }
+    }
+}
